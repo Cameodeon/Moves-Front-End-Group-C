d5d014e1f1c482e468d0b74db02c2def46ab41c7
Khang Nguyen <nnknguyen@myseneca.ca>
2019-08-13 13:26:49 +0200

    refractor navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Home from './Home';
+import Navbar from './Navbar';
 import NotFound from './NotFound';
 import Content from './Content';
 import EmergencyContact from './EmergencyContact';
@@ -87,77 +88,10 @@ const Header = (props) => {
         <h1 className="appTitle">MO:VES</h1>
         <p className="appDescription">{dict.description}</p>
         <Link id="emergencyButton" className="btn btn-danger" to="/emergency">
+          <FontAwesomeIcon icon={['fas', 'phone-alt']} style={{color: 'white'}} size="lg" /> &nbsp;
           {dict.call}
         </Link>
       </div>
     </header>
   );
-}
-
-const Navbar = (props) => {
-    var { dict, changeLanguage } = props;
-    return (
-      <nav className="navbar navbar-expand-lg bg-dark navbar-dark" id="customNavbar">
-        <button className="navbar-toggler btn-lg" type="button" data-toggle="collapse" data-target="#menuBar">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <button className="navbar-toggler navbar-button" type="button" data-toggle="collapse" data-target="#languageSelector">
-          {dict.selectLang}
-        </button>
-
-        <div className="collapse navbar-collapse" id="menuBar" data-parent="#customNavbar">
-          <ul className="navbar-nav text-center">
-            <li className="nav-item first-nav-item">
-              <Link className="navbar-brand nav-link btn-dark" to="/">
-                <FontAwesomeIcon icon={['fas', 'home']} style={{color: 'white'}} size="lg" /> &nbsp;
-                {dict.home}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">{dict.help}</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">{dict.info}</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">{dict.about}</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">{dict.contact}</Link>
-            </li>
-            <li className="nav-item">
-              <Link id="loginBtn" className="navbar-brand nav-link btn-dark" to="/login">
-                <FontAwesomeIcon icon={['fas', 'users']} style={{color: 'white'}} size="lg" /> &nbsp;
-                {dict.login}
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="collapse navbar-collapse" id="languageSelector" data-parent="#customNavbar">
-          <ul className="navbar-nav ml-auto text-center">
-            <li className="nav-item first-nav-item">
-              <button className="nav-link btn" onClick={() => changeLanguage("en-CA")}>
-                <span className="flag-icon flag-icon-ca"></span> &nbsp;
-                English (Canada)
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="nav-link btn" onClick={() => changeLanguage("vn")}>
-                <span className="flag-icon flag-icon-vn"></span> &nbsp;
-                Tiếng Việt
-              </button>
-            </li>
-            {/* <li className="nav-item">
-              <Link className="nav-link" to="#">{dict.danish}</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="#">{dict.french}</Link>
-            </li> */}
-          </ul>
-        </div>
-      </nav>
-    );
-
 }
---
+++ src/Navbar.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Navbar.css';
+
+class Navbar extends Component {
+
+    render() {
+        var { dict, changeLanguage } = this.props;
+        return (
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark" id="customNavbar">
+            <button className="navbar-toggler btn-lg" type="button" data-toggle="collapse" data-target="#menuBar">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            
+            <button className="navbar-toggler navbar-button" type="button" data-toggle="collapse" data-target="#languageSelector">
+                {dict.selectLang}
+            </button>
+
+            <div className="collapse navbar-collapse" id="menuBar" data-parent="#customNavbar">
+                <ul className="navbar-nav text-center">
+                    <li className="nav-item first-nav-item" id="homeBtn">
+                        <Link className="navbar-brand nav-link btn-dark" to="/">
+                            <FontAwesomeIcon icon={['fas', 'home']} style={{color: 'white'}} size="lg" /> &nbsp;
+                            {dict.home}
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">{dict.help}</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">{dict.info}</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">{dict.about}</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">{dict.contact}</Link>
+                    </li>
+                    <li className="nav-item" id="loginBtn">
+                        <Link className="navbar-brand nav-link btn-dark" to="/login">
+                            <FontAwesomeIcon icon={['fas', 'users']} style={{color: 'white'}} size="lg" /> &nbsp;
+                            {dict.login}
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="collapse navbar-collapse" id="languageSelector" data-parent="#customNavbar">
+                <ul className="navbar-nav ml-auto text-center">
+                    <li className="nav-item first-nav-item">
+                        <button className="nav-link btn" onClick={() => changeLanguage("en-CA")}>
+                            <span className="flag-icon flag-icon-ca"></span> &nbsp;
+                            English (Canada)
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link btn" onClick={() => changeLanguage("vn")}>
+                            <span className="flag-icon flag-icon-vn"></span> &nbsp;
+                            Tiếng Việt
+                        </button>
+                    </li>
+                    {/* <li className="nav-item">
+                    <Link className="nav-link" to="#">{dict.danish}</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="#">{dict.french}</Link>
+                    </li> */}
+                </ul>
+            </div>
+        </nav>
+        );
+    }
+
+}
+
+export default Navbar;
