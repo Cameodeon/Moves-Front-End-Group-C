8c81ce8a6f59d9031fbdf91f26d4b58bfd216338
Andre Paiement <apaiement@myseneca.ca>
2019-08-14 11:38:36 +0200

    Added PWA features

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{json,png,jpg,html,js,css,svg}"
+  ],
+  "swSrc": "public/sw-custom.js",
+  "swDest": "public/service-worker.js"
+};
