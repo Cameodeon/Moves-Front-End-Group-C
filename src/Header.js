6cdf9768bbd1aa75c730a7a05d9810595750b6a8
Khang Nguyen <nnknguyen@myseneca.ca>
2019-08-15 22:54:49 +0200

    fix UI and routing bugs

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Auth from './Auth';
 import './Header.css';
 
 class Header extends Component {
 
-    isLoggedIn() {
-        return localStorage.getItem('access_token') ? true : false;
-    }
-
     render() {
-        var dict = this.props.dict;
+        var { dict } = this.props;
         return(
           <header>
             <div className="header text-center">
                 <h1 className="appTitle">MO:VES</h1>
                 <p className="appDescription">{dict.description}</p>
                 {
-                this.isLoggedIn() ?
+                Auth.isAuth() ?
                 <Link id="emergencyButton" className="btn btn-danger" to="/emergency">
                     <FontAwesomeIcon icon={['fas', 'phone-alt']} style={{color: 'white'}} size="lg" /> &nbsp;
                     {dict.call}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter  } from 'react-router-dom';
+import { BrowserRouter  } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,9 +10,9 @@ import 'flag-icon-css/css/flag-icon.min.css';
 import './fontawesome.js';
 
 ReactDOM.render(
-    <HashRouter >
+    <BrowserRouter>
         <App />
-    </HashRouter >,
+    </BrowserRouter >,
     document.getElementById('root')
 );
 
