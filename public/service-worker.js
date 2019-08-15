e27b5f488ada75c111fb39fc9df1149ab58541b0
Khang Nguyen <nnknguyen@myseneca.ca>
2019-08-15 17:30:23 +0200

    fix web service url

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,7 +47,7 @@ function readTable(table, key) {
 }
 
 workbox.routing.registerRoute(
-    new RegExp('https://movessw-teamc-baa.herokuapp.com/api/phoneNumber/.*$'),
+    new RegExp('https://movesws-teamc-baa.herokuapp.com/api/phoneNumber/.*$'),
     ({ url, event }) => {
         console.log(url.pathname);
         return fetch(event.request)
@@ -70,7 +70,7 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-    new RegExp('https://movessw-teamc-baa.herokuapp.com/api/phoneNumber/.*$'),
+    new RegExp('https://movesws-teamc-baa.herokuapp.com/api/phoneNumber/.*$'),
     ({ url, event }) => {
         console.log(url.pathname);
         return fetch(event.request)
